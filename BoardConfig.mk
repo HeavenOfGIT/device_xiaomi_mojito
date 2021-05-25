b70ed84b7b2e3042f67b6346bf77063335acaf2e
boedhack99 <boedhack@gmail.com>
2021-05-25 08:17:43 +0700

    Permissive temp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,8 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE+= 
 #TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 #TARGET_COMPILE_WITH_MSM_KERNEL := true
