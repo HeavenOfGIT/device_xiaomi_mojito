cb84cbefd6d94db946b2ea30dae9d34c064a816e
boedhack99 <boedhack@gmail.com>
2021-05-30 17:13:11 +0700

    trying to fix passing from redfin

--- init/init_mojito.cpp
+++ init/init_mojito.cpp
@@ -1,6 +1,7 @@
 /*
-   Copyright (C) 2020 The LineageOS Project.
-
+   Copyright (c) 2015, The Linux Foundation. All rights reserved.
+   Copyright (C) 2016 The CyanogenMod Project.
+   Copyright (C) 2019-2020 The LineageOS Project.
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -13,7 +14,6 @@
     * Neither the name of The Linux Foundation nor the names of its
       contributors may be used to endorse or promote products derived
       from this software without specific prior written permission.
-
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
@@ -27,19 +27,20 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <fstream>
+#include <unistd.h>
 #include <vector>
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/_system_properties.h>
-#include <sys/sysinfo.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
 
 using android::base::GetProperty;
+using android::base::SetProperty;
+using std::string;
 
 std::vector<std::string> ro_props_default_source_order = {
     "",
@@ -51,133 +52,56 @@ std::vector<std::string> ro_props_default_source_order = {
     "vendor.",
 };
 
-void property_override(char const prop[], char const value[], bool add = true)
-{
+void property_override(char const prop[], char const value[], bool add = true) {
     prop_info *pi;
 
-    pi = (prop_info *) __system_property_find(prop);
+    pi = (prop_info *)__system_property_find(prop);
     if (pi)
         __system_property_update(pi, value, strlen(value));
     else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void set_ro_build_prop(const std::string &prop, const std::string &value) {
-    for (const auto &source : ro_props_default_source_order) {
+void vendor_load_properties() {
+    const auto set_ro_build_prop = [](const std::string &source,
+                                      const std::string &prop,
+                                      const std::string &value) {
         auto prop_name = "ro." + source + "build." + prop;
-        if (source == "")
-            property_override(prop_name.c_str(), value.c_str());
-        else
-            property_override(prop_name.c_str(), value.c_str(), false);
-    }
-};
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
 
-void set_ro_product_prop(const std::string &prop, const std::string &value) {
-    for (const auto &source : ro_props_default_source_order) {
+    const auto set_ro_product_prop = [](const std::string &source,
+                                        const std::string &prop,
+                                        const std::string &value) {
         auto prop_name = "ro.product." + source + prop;
         property_override(prop_name.c_str(), value.c_str(), false);
-    }
-};
+    };
+    
+    property_override("ro.build.fingerprint", "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ2A.210305.006 7119741 release-keys");
+   
+    std::string device, model;
 
-/* From Magisk@jni/magiskhide/hide_utils.c */
-static const char *snet_prop_key[] = {
-    "ro.boot.vbmeta.device_state",
-    "ro.boot.verifiedbootstate",
-    "ro.boot.flash.locked",
-    "ro.boot.veritymode",
-    "ro.boot.warranty_bit",
-    "ro.warranty_bit",
-    "ro.debuggable",
-    "ro.secure",
-    "ro.build.type",
-    "ro.build.tags",
-    "ro.build.selinux",
-    NULL
-};
+    std::string hwname = GetProperty("ro.boot.hwname", "");
 
- static const char *snet_prop_value[] = {
-    "locked",
-    "green",
-    "1",
-    "enforcing",
-    "0",
-    "0",
-    "0",
-    "1",
-    "user",
-    "release-keys",
-    "1",
-    NULL
-};
-
- static void workaround_snet_properties() {
-
-     // Hide all sensitive props
-    for (int i = 0; snet_prop_key[i]; ++i) {
-        property_override(snet_prop_key[i], snet_prop_value[i]);
-    }
-
-}
-
-void load_dalvik_properties() {
-    struct sysinfo sys;
-
-    sysinfo(&sys);
-    if (sys.totalram < 4096ull * 1024 * 1024) {
-        // from - phone-xhdpi-6144-dalvik-heap.mk
-        property_override("dalvik.vm.heapstartsize", "16m");
-        property_override("dalvik.vm.heapgrowthlimit", "256m");
-        property_override("dalvik.vm.heapsize", "512m");
-        property_override("dalvik.vm.heapmaxfree", "32m");
-    }
-
-    property_override("dalvik.vm.heaptargetutilization", "0.5");
-    property_override("dalvik.vm.heapminfree", "8m");
-}
-
-void vendor_load_properties() {
-    std::string region;
-    std::string sku;
-    region = GetProperty("ro.boot.hwc", "GLOBAL");
-    sku = GetProperty("ro.boot.product.hardware.sku", "");
-
-    std::string model;
-    std::string device;
-    std::string fingerprint;
-    std::string description;
-    std::string marketname;
-    std::string mod_device;
-
-    if (region == "GLOBAL") {
-        model = "M2101K7AG";
+    if (hwname == "sunny") {
         device = "sunny";
-        fingerprint = "Redmi/sunny_global/sunny:11/RKQ1.201022.002/V12.0.2.0.RKGMIXM:user/release-keys";
-        description = "sunny_global-user 11 RKQ1.201022.002 V12.0.2.0.RKGMIXM release-keys";
-        marketname = "Redmi Note 10";
-        mod_device = "sunny_global";
-    } else if (region == "INDIA") {
-        if (sku == "std") {
-            model = "M2101K7AG";
-            device = "mojito";
-            fingerprint = "Redmi/mojito/mojito:11/RKQ1.201022.002/V12.0.7.0.RKGMIXM:user/release-keys";
-            description = "mojito-user 11 RKQ1.201022.002 V12.0.7.0.RKGMIXM release-keys";
-            marketname = "Redmi Note 10";
-            mod_device = "mojito";
-            }
-        }
-
-    // SafetyNet workaround
-    fingerprint = "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys";
-    description = "redfin-user 11 RQ2A.210305.006 7119741 release-keys";
-    workaround_snet_properties();
-
-    set_ro_build_prop("fingerprint", fingerprint);
-    set_ro_product_prop("device", device);
-    set_ro_product_prop("model", model);
-    property_override("ro.product.marketname", marketname.c_str());
-    property_override("ro.build.description", description.c_str());
-    if (mod_device != "") {
-        property_override("ro.product.mod_device", mod_device.c_str());
+        model = "M2101K7AG";
+    } else {
+        device = "mojito";
+        model = "M2101K7AG";
     }
-    load_dalvik_properties();
+
+    // Override all partitions' props
+    string prop_partitions[] = { "", "odm.", "product.", "system.", "system_ext.", "vendor." };
+    for (const string &prop : prop_partitions) {
+        property_override(string("ro.product.") + prop + string("board"), device);
+        property_override(string("ro.product.") + prop + string("device"), device);
+        property_override(string("ro.product.") + prop + string("name"), device);
+        property_override(string("ro.product.") + prop + string("model"), model);
+        property_override(string("ro.") + prop + string("build.product"), device);
+    }
+
+    // Set hardware SKU prop
+    property_override("ro.boot.product.hardware.sku", device);
 }
