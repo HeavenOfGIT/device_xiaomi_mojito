51997229177326199e291ae1fa0b4dded29c945d
boedhack99 <boedhack@gmail.com>
2021-06-02 15:11:21 +0700

    Add temporary XiaomiParts

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2021 The PixelExperience Project
+# Copyright (C) 2021 Extended Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_mojito.mk
+    $(LOCAL_DIR)/aosp_mojito.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_mojito-user \
--- device.mk
+++ device.mk
@@ -159,7 +159,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     AospFrameworkResOverlay \
