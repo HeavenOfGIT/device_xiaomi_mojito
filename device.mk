badea36fdd3608897d5da55d3abb2125d39aaedb
boedhack99 <boedhack@gmail.com>
2021-06-15 16:43:53 +0700

    mojito: update some files

--- device.mk
+++ device.mk
@@ -228,6 +228,16 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    libkeymaster_messages.vendor:64
+
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6150
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # Framework detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
