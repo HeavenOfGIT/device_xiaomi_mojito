4a169fe9c34514a84491e826253c30b42cf63f82
boedhack99 <boedhack@gmail.com>
2021-06-20 12:02:01 +0700

    mojito: add system properties

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -201,8 +201,9 @@ TARGET_TAP_TO_WAKE_NODE := "/dev/input/event3"
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
